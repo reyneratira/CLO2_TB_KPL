4002a68f48f7380be83b768f6a5eba93f5811f00
Alvin Bagus Firmansyah <rusmiantofajri@gmail.com>
2025-04-29 13:35:28 +0700

    Step 3 - Implementasi Simulasi Proses Layanan

---
+++ Modul_Simulasi_AlvinBagusF/SimulasiProsesLayanan/SimulasiProsesLayanan/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SimulasiProsesLayanan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceQueue queue = new ServiceQueue();
+            queue.AddCustomer(new Customer("Alvin"));
+            queue.AddCustomer(new Customer("Ilham"));
+            queue.AddCustomer(new Customer("Reyner"));
+            queue.AddCustomer(new Customer("Aulia"));
+
+            queue.ShowQueue();
+            queue.ProcessNext();
+            queue.ProcessNext();
+            queue.ProcessNext();
+            queue.ProcessNext();
+            queue.ProcessNext();
+        }
+    }
+}
---
+++ Modul_Simulasi_AlvinBagusF/SimulasiProsesLayanan/SimulasiProsesLayanan/ServiceQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimulasiProsesLayanan
+{
+    public class ServiceQueue
+    {
+        private Queue<Customer> queue = new Queue<Customer>();
+
+        public void AddCustomer(Customer customer)
+        {
+            queue.Enqueue(customer);
+            Console.WriteLine($"{customer.Name} masuk dalam antrean.");
+        }
+
+        public void ProcessNext()
+        {
+            if (queue.Count > 0)
+            {
+                var next = queue.Dequeue();
+                Console.WriteLine($"Melayani {next.Name}...");
+            }
+            else
+            {
+                Console.WriteLine("Antrean kosong.");
+            }
+        }
+
+        public void ShowQueue()
+        {
+            Console.WriteLine("Antrean saat ini:");
+            foreach (var c in queue)
+            {
+                Console.WriteLine($"- {c.Name}");
+            }
+        }
+    }
+}
