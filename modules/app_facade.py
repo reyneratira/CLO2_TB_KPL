501b92a5a411b243ca2216bf9de4ec3e2ecfd7d8
Reyner Atira Prasetyo <tneon590@gmail.com>
2025-06-03 22:09:27 +0700

    feat : added app_facade module with facade pattern implementation

---
+++ modules/app_facade.py
@@ -0,0 +1,40 @@
+from modules.queue_manager import queue_manager
+from modules.config_loader import load_services
+from modules.registration import defensive_code, validation_code
+from modules.simulator import ServiceSimulator, NormalService, FastService
+
+class AppFacade:
+    def __init__(self, config_path='config/services.json'):
+        self.services = load_services(config_path)
+        self.qm = queue_manager(self.services)
+        self.simulator = ServiceSimulator(NormalService())
+
+    def list_services(self):
+        return self.services
+
+    def add_customer(self, name, code):
+        if not validation_code(code):
+            raise ValueError("Kode layanan harus dalam format LXX")
+        customer = defensive_code(name, code)
+        if code not in self.services:
+            raise ValueError("Kode layanan tidak tersedia")
+        self.qm.add_to_queue(code, name)
+
+    def get_queue(self):
+        return list(self.qm.queue)
+
+    def serve_next_customer(self):
+        if self.qm.is_empty():
+            raise IndexError("Antrian kosong")
+        customer = self.qm.get_next_customer()
+        service_name = self.qm.get_service_name(customer['service_code'])
+        duration = self.qm.get_service_time(customer['service_code'])
+        self.simulator.run(customer, service_name, duration)
+
+    def set_simulation_mode(self, mode='normal'):
+        if mode == 'normal':
+            self.simulator.set_strategy(NormalService())
+        elif mode == 'fast':
+            self.simulator.set_strategy(FastService())
+        else:
+            raise ValueError("Mode tidak dikenal")
